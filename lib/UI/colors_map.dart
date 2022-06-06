b8b5a5603bc2aee408a94d77985b8e7f6d313c01
eNotEnoughBeer <gorlov.nico@gmail.com>
2022-06-06 11:43:26 +0300

    some modifications and improvements

--- lib/UI/colors_map.dart
+++ lib/UI/colors_map.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-const unusedColor = Color(0xFF6B6168);
-const presentOnCorrectPositionColor = Color(0xFF3DA35D);
-const presentColor = Color(0xFFCCA300);
-const errorColor = Color(0xFFE13D3D);
-const cardBorder = Color(0xFF7D737E);
-const textColor = Color(0xFFD2CBD2);
-const absentColor = Color(0xFF140029);
-const backgroundColor = Color(0xFF372442);
+const unusedColor = Color(0xFF8C7B45);
+const presentOnCorrectPositionColor = Color(0xFF5CBA3E);
+const presentColor = Color(0xFFDFA700);
+const errorColor = Color(0xFFD32A2A);
+const cardBorder = Color(0xFF9B8C5B);
+const textColor = Color(0xFFDCD0AB);
+const absentColor = Color(0xFF2B0010);
+const backgroundColor = Color(0xFF5A3A23);
